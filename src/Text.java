1edb599bf595feb25b6f7e28331a02fcfdd82728
vanshika <vanshikavsharma88@gmail.com>
2023-05-17 16:10:26 +0530

    few more lines of code is added in BranchX

--- src/Text.java
+++ src/Text.java
@@ -5,5 +5,14 @@
  * @author dell
  *
  */
-module GitJavaProject {
+public class Text{
+	public static void main(String []args) {
+		String hungry = "yes";
+		if(hungry == "yes")
+			System.out.println("eat something ...");
+		else {
+			System.out.println("don't eat");
+		}
+			
+	}
 }
